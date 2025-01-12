60324bad56cdb42ee2a93f63e1c25c51f8a0be9b
danielbergan <daniel.bergan86@gmail.com>
2025-01-11 19:01:45 -0800

    Started bedtime stories program

---
+++ src/BedtimeStories.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class bedtime_stories {
+    public static void main(String[] args)	
+
+	
+}
---
+++ src/Book.java
@@ -0,0 +1,18 @@
+public class Book {
+    private String title;
+    private int length;
+
+    public Book(String title, int length) {
+	this.title = title;
+	this.length = length;
+    }
+
+    public String getTitle() {
+	return title;
+    }
+
+    public int getLength() {
+	return length;
+    }
+}
+	
